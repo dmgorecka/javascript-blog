e89107d2dbf11aafe621b369c92845bb47d93a69
Daria Górecka <dmgorecka@gmail.com>
2021-07-02 11:03:15 +0200

    Change task runner

--- js/script.js
+++ js/script.js
@@ -81,7 +81,7 @@ function generateTitleLinks(customerSelector = ''){
     titleList.innerHTML = html
 
     const links = document.querySelectorAll('.titles a');
-    console.log (links)
+    console.log(links)
       for(let link of links){
       link.addEventListener('click', titleClickHandler);
     }
